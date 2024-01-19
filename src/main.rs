1ab7ba5128adbaa6e2c1017bb6319153d02f088b
ASoldo <soldo.andrej@gmail.com>
2024-01-19 03:23:03 +0100

    Add postgrest, reqwest and integrate it with supabase

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use actix::{Actor, ActorContext, AsyncContext, StreamHandler};
+use actix_web::cookie::time::Duration;
 use actix_web::cookie::Cookie;
 use actix_web::http::header::CACHE_CONTROL;
+use actix_web::middleware::ErrorHandlerResponse::Response;
 use actix_web::middleware::Logger;
 use actix_web::web::{Bytes, Data};
-use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    delete, get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
+};
 use actix_web_actors::ws;
 use ammonia::clean;
 use futures::lock::Mutex;
 use futures::stream::StreamExt;
+use postgrest::Postgrest;
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::time::Duration;
+use std::collections::HashMap;
 use tera::{Context, Tera};
 use tokio::time::interval;
 use tokio_stream::wrappers::IntervalStream;
@@ -24,6 +31,66 @@ struct TeraTemplates {
     tera: Tera,
 }
 
+#[derive(Serialize, Deserialize)]
+struct LoginRequest {
+    email: String,
+    password: String,
+}
+
+#[derive(Deserialize)]
+struct SupabaseResponse {
+    // Add fields based on Supabase's response, like `access_token`
+    access_token: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: String,
+    name: String,
+    email: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SupabaseLoginResponse {
+    access_token: String,
+    token_type: String,
+    expires_in: i64,
+    expires_at: i64,
+    refresh_token: String,
+    user: SupabaseUser,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SupabaseUser {
+    id: String,
+    aud: String,
+    role: String,
+    email: String,
+    email_confirmed_at: Option<String>,
+    phone: String,
+    confirmation_sent_at: Option<String>,
+    confirmed_at: Option<String>,
+    last_sign_in_at: Option<String>,
+    app_metadata: HashMap<String, serde_json::Value>,
+    user_metadata: HashMap<String, serde_json::Value>,
+    identities: Vec<SupabaseIdentity>,
+    created_at: String,
+    updated_at: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SupabaseIdentity {
+    identity_id: String,
+    id: String,
+    user_id: String,
+    identity_data: HashMap<String, String>,
+    provider: String,
+    last_sign_in_at: Option<String>,
+    created_at: Option<String>,
+    updated_at: Option<String>,
+    email: Option<String>,
+}
+
 impl Actor for ChatSocket {
     type Context = ws::WebsocketContext<Self>;
 }
@@ -36,13 +103,15 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ChatSocket {
                     if let Some(chat_message) = parsed["chat_message"].as_str() {
                         let sanitized_message = clean(&chat_message);
                         ctx.text(format!(
-                            "<div id=\"chat_room\" hx-swap-oob=\"beforeend\">{}<br></div>\n
-<form id=\"form\" ws-send hx-swap-oob=\"morphdom\">
-    <label>
-        <input id=\"typed_message\" name=\"chat_message\" type=\"text\" placeholder=\"Type your message...\" autofocus autocomplete required minlength=\"5\" maxlength=\"20\" />
-    </label>
-    <button type=\"submit\">submit</button>
-</form>\n",
+                            "
+                            <div id=\"chat_room\" hx-swap-oob=\"beforeend\">{}<br></div>\n
+                            <form id=\"form\" ws-send hx-swap-oob=\"morphdom\">
+                                <label>
+                                    <input id=\"typed_message\" name=\"chat_message\" type=\"text\" placeholder=\"Type your message...\" autofocus autocomplete required minlength=\"5\" maxlength=\"20\" />
+                                </label>
+                                <button type=\"submit\">submit</button>
+                            </form>\n
+                            ",
                             sanitized_message
                         ));
                     }
@@ -63,6 +132,102 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ChatSocket {
         ctx.text("Hello world!");
         println!("Connected: {:?}", ctx.address());
     }
+}
+
+#[post("/logout")]
+async fn logout() -> impl Responder {
+    // Create a cookie to clear the authentication
+    let clear_cookie_access_token = Cookie::build("access_token", "")
+        .http_only(true)
+        .secure(true)
+        .max_age(Duration::minutes(0)) // Set max-age to zero
+        .finish();
+
+    let clear_cookie_refresh_token = Cookie::build("refresh_token", "")
+        .http_only(true)
+        .secure(true)
+        .max_age(Duration::minutes(0)) // Set max-age to zero
+        .finish();
+    // Return the login form HTML
+    HttpResponse::Ok()
+        .cookie(clear_cookie_access_token) // Set the cookie in the response to clear it
+        .cookie(clear_cookie_refresh_token) // Set the cookie in the response to clear it
+        .body(format!(
+            "
+            <form hx-boost=\"true\" id=\"form\" hx-post=\"/login\">
+              <input type=\"text\" name=\"email\" placeholder=\"email\" />
+              <input type=\"password\" name=\"password\" placeholder=\"password\" />
+              <button type=\"submit\">Login</button>
+              <h1>Logged out</h1>
+            </form>
+            "
+        ))
+}
+
+#[post("/login")]
+async fn login(credentials: web::Form<LoginRequest>) -> impl Responder {
+    let client = Client::new();
+
+    let creds_json = match serde_json::to_string(&credentials.into_inner()) {
+        Ok(json) => json,
+        Err(_) => return HttpResponse::InternalServerError().json("Error serializing credentials"),
+    };
+
+    let res = client.post("https://kxbzixfkcjexfwfacnzq.supabase.co/auth/v1/token?grant_type=password")
+    .header("apikey", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4YnppeGZrY2pleGZ3ZmFjbnpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ5NTQzODEsImV4cCI6MjAwMDUzMDM4MX0.Jl5GMoQSyVVgOFAHRIyCEFFgsGe1YahNVCaCjehO0hw")
+    .header("Content-Type", "application/json")
+    .body(creds_json).send().await;
+
+    match res {
+        Ok(response) => {
+            if response.status().is_success() {
+                match response.json::<SupabaseLoginResponse>().await {
+                    Ok(supabase_res) => {
+                        let cookie_access_token =
+                            Cookie::new("access_token", supabase_res.access_token);
+                        let cookie_refresh_token =
+                            Cookie::new("refresh_token", supabase_res.refresh_token);
+
+                        dbg!(cookie_access_token.clone());
+                        dbg!(supabase_res.user.email.clone());
+                        HttpResponse::Ok()
+                            .cookie(cookie_access_token)
+                            .cookie(cookie_refresh_token)
+                            .body(format!(
+                                "
+                                <form hx-boost=\"true\" id=\"form\" hx-post=\"/logout\">
+                                    <button type=\"submit\">Logout</button>
+                                <h1>Logged in as {}</h1>
+                                </form>
+                                ",
+                                supabase_res.user.email
+                            ))
+                    }
+                    Err(_) => HttpResponse::InternalServerError().finish(),
+                }
+            } else {
+                dbg!(response.status().is_success());
+                HttpResponse::Ok().body(format!(
+                    "
+                    <form hx-boost=\"true\" id=\"form\" hx-post=\"/login\">
+                        <input type=\"text\" name=\"email\" value=\"\" placeholder=\"email\" />
+                        <input type=\"password\" name=\"password\" value=\"\" placeholder=\"password\" />
+                        <button type=\"submit\">Login</button>
+                        <h1>Invalid credentials</h1>
+                    </form>
+                    "
+                ))
+            }
+        }
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[get("/leaderboard")]
+async fn get_leaderboard(sb: Data<Postgrest>) -> impl Responder {
+    let resp = sb.from("leaderboard").select("*").execute().await.unwrap();
+    let body = resp.text().await.unwrap();
+    HttpResponse::Ok().json(body.parse::<Value>().unwrap())
 }
 
 #[get("/ws/")]
@@ -133,7 +298,7 @@ async fn hello(name: web::Path<String>) -> impl Responder {
 #[get("/events")]
 async fn events() -> impl Responder {
     let server_sent_event = move || {
-        let interval = IntervalStream::new(interval(Duration::from_secs(1)));
+        let interval = IntervalStream::new(interval(std::time::Duration::from_secs(1)));
         interval.map(move |_| Ok::<_, Error>(Bytes::from("id:1\ndata: Server-sent event \n\n")))
     };
 
@@ -153,10 +318,18 @@ async fn main() -> std::io::Result<()> {
         tera: Tera::new("templates/**/*").expect("Problem setting up Tera"),
     });
 
+    let supabase = Postgrest::new("https://kxbzixfkcjexfwfacnzq.supabase.co/rest/v1")
+        .insert_header("apikey", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4YnppeGZrY2pleGZ3ZmFjbnpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ5NTQzODEsImV4cCI6MjAwMDUzMDM4MX0.Jl5GMoQSyVVgOFAHRIyCEFFgsGe1YahNVCaCjehO0hw");
+    let sb = Data::new(supabase);
+
     HttpServer::new(move || {
         App::new()
+            .app_data(sb.clone())
             .app_data(counter.clone())
             .app_data(tera_templates.clone())
+            .service(login)
+            .service(logout)
+            .service(get_leaderboard)
             .service(index)
             .service(hello)
             .service(events)
