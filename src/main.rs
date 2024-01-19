fbc8b4c310fda55d4b0d4c8eb9945f2d6f815014
ASoldo <soldo.andrej@gmail.com>
2024-01-20 00:27:33 +0100

    Update: Add profile.release
    -Build from 22Mb to 8.7Mb size
    -minor changes

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,12 @@ use actix::{Actor, ActorContext, AsyncContext, StreamHandler};
 use actix_web::cookie::time::Duration;
 use actix_web::cookie::Cookie;
 use actix_web::http::header::CACHE_CONTROL;
-use actix_web::middleware::ErrorHandlerResponse::Response;
 use actix_web::middleware::Logger;
 use actix_web::web::{Bytes, Data};
-use actix_web::{
-    delete, get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
-};
+use actix_web::{get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web_actors::ws;
 use ammonia::clean;
+use dotenv::dotenv;
 use futures::lock::Mutex;
 use futures::stream::StreamExt;
 use postgrest::Postgrest;
@@ -20,6 +18,7 @@ use std::collections::HashMap;
 use tera::{Context, Tera};
 use tokio::time::interval;
 use tokio_stream::wrappers::IntervalStream;
+extern crate dotenv;
 
 struct ChatSocket;
 
@@ -35,12 +34,6 @@ struct TeraTemplates {
 struct LoginRequest {
     email: String,
     password: String,
-}
-
-#[derive(Deserialize)]
-struct SupabaseResponse {
-    // Add fields based on Supabase's response, like `access_token`
-    access_token: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -239,10 +232,7 @@ async fn ws_index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse
 async fn index(tera: Data<TeraTemplates>) -> impl Responder {
     let context = Context::new();
 
-    let rendered = tera
-        .tera
-        .render("home.html", &context)
-        .expect("Failed to render template.");
+    let rendered = tera.tera.render("home.html", &context).expect("Failed to render template.");
     HttpResponse::Ok().body(rendered)
 }
 
@@ -250,10 +240,7 @@ async fn index(tera: Data<TeraTemplates>) -> impl Responder {
 async fn content(tera: Data<TeraTemplates>) -> impl Responder {
     let context = Context::new();
 
-    let rendered = tera
-        .tera
-        .render("content.html", &context)
-        .expect("Failed to render template.");
+    let rendered = tera.tera.render("content.html", &context).expect("Failed to render template.");
     HttpResponse::Ok().body(rendered)
 }
 
@@ -269,17 +256,14 @@ async fn get_comp(counter: Data<Counter>, tera: Data<TeraTemplates>) -> impl Res
     context.insert("last_name", &last_name);
     context.insert("counter", &*counter);
 
-    let rendered = tera
-        .tera
-        .render("comp.html", &context)
-        .expect("Failed to render template.");
+    let rendered = tera.tera.render("comp.html", &context).expect("Failed to render template.");
 
     HttpResponse::Ok().body(rendered)
 }
 
 #[get("/cookie")]
 async fn cookie(req: HttpRequest, tera: Data<TeraTemplates>) -> impl Responder {
-    let mut counter = if let Some(cookie) = req.cookie("counter") {
+    let counter = if let Some(cookie) = req.cookie("counter") {
         cookie.value().parse::<i32>().unwrap_or(0) + 1
     } else {
         0
@@ -294,10 +278,8 @@ async fn cookie(req: HttpRequest, tera: Data<TeraTemplates>) -> impl Responder {
     context.insert("last_name", "Kowalski");
     context.insert("user_counter", &counter.to_string());
 
-    let rendered = tera
-        .tera
-        .render("comp-user.html", &context)
-        .expect("Failed to render template.");
+    let rendered =
+        tera.tera.render("comp-user.html", &context).expect("Failed to render template.");
     response.body(rendered)
 }
 
@@ -321,18 +303,20 @@ async fn events() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let counter = Data::new(Counter {
-        count: Mutex::new(0),
-    });
+    dotenv().ok();
 
     let tera_templates = Data::new(TeraTemplates {
         tera: Tera::new("templates/**/*").expect("Problem setting up Tera"),
     });
 
-    let supabase = Postgrest::new("https://kxbzixfkcjexfwfacnzq.supabase.co/rest/v1")
-        .insert_header("apikey", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4YnppeGZrY2pleGZ3ZmFjbnpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ5NTQzODEsImV4cCI6MjAwMDUzMDM4MX0.Jl5GMoQSyVVgOFAHRIyCEFFgsGe1YahNVCaCjehO0hw");
+    let supabase_url = std::env::var("SUPABASE_URL").expect("SUPABASE_URL not set");
+    let supabase_public_key =
+        std::env::var("SUPABASE_PUBLIC_KEY").expect("SUPABASE_PUBLIC_KEY not set");
+
+    let supabase = Postgrest::new(supabase_url).insert_header("apikey", supabase_public_key);
     let sb = Data::new(supabase);
 
+    let counter = Data::new(Counter { count: Mutex::new(0) });
     HttpServer::new(move || {
         App::new()
             .app_data(sb.clone())
